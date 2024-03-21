b4e43bfd1bb856578dbe0c2b1dfd9c46f564a641
zhaoyiming <114985982+AndyFred0930@users.noreply.github.com>
2024-03-21 18:43:06 +0800

    Update AList.java

--- lec7_lists4/DIY/AList.java
+++ lec7_lists4/DIY/AList.java
@@ -11,25 +11,34 @@ public class AList {
 
     /** Inserts X into the back of the list. */
     public void addLast(int x) {
+        AList[size()]=x;
     }
 
     /** Returns the item from the back of the list. */
     public int getLast() {
-        return 0;        
+        return AList[size()-1];        
     }
     /** Gets the ith item in the list (0 is the front). */
     public int get(int i) {
-        return 0;        
+        return AList[i-1];
+         
     }
 
     /** Returns the number of items in the list. */
     public int size() {
-        return 0;        
+        int len=0;
+        while( AList[len]!=null)
+            {
+                len++;
+            }
+        return len;
     }
 
     /** Deletes item from back of the list and
       * returns deleted item. */
     public int removeLast() {
-        return 0;
+        int last=AList[size()-1];
+        size()--;
+        return last;
     }
 } 
